f639c98c9a74d370d12c29692b08283ff6485d50
Milton Andres Castillo Garcia <milton.castillog@gmail.com>
2019-10-22 22:42:15 +0200

    css

--- js/collisions.js
+++ js/collisions.js
@@ -1,3 +1,9 @@
-const check_collision = () => {
-	
+const check_collision = (bullet) => {
+    document.querySelectorAll(".wall").forEach((elem)=>{
+
+        if(elem.getBoundingClientRect().bottom>300){
+            destroy_wall(elem)
+        };
+    
+    })	
 }
--- js/walls.js
+++ js/walls.js
@@ -1,7 +1,15 @@
-const destroy_wall = (position) => {
-
+const destroy_wall = (elem) => {
+    elem.className="wall-part destroyed";
 }
 
 const construct_walls = () => {
-
+    const blocksPerColumn = 4;
+    const blocksPerRow = 4;
+    document.querySelectorAll(".wall").forEach((elem)=>{
+        for(let i=0; i<4*4; i++){
+            const newDiv = document.createElement("div")
+            newDiv.className = "wall-part destructable";
+            elem.appendChild(newDiv)
+        }
+    });
 }
